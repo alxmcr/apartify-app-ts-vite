49b9847147ec8e44701f95d316048910903291df
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-16 14:25:57 -0400

    + BallsLoader

---
+++ src/components/loaders/BallsLoader/index.tsx
@@ -0,0 +1,16 @@
+import "./BallsLoader.scss";
+
+export const BallsLoader = () => {
+  return (
+    <section className="balls">
+      <span className="balls__ball balls__ball--one"></span>
+      <span className="balls__ball balls__ball--two"></span>
+      <span className="balls__ball balls__ball--three"></span>
+      <span className="balls__ball balls__ball--four"></span>
+      <span className="balls__ball balls__ball--five"></span>
+      <span className="balls__ball balls__ball--six"></span>
+      <span className="balls__ball balls__ball--seven"></span>
+      <span className="balls__ball balls__ball--eight"></span>
+    </section>
+  );
+};
