fdefd44a39693c08dac92dda56d7a0c764c8946e
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-16 14:04:21 -0400

    Create custom hooks

--- src/hooks/useLocationState.ts
+++ src/hooks/useLocationState.ts
@@ -1,13 +1,13 @@
 import { useEffect, useState } from "react"
-import { City } from "../types/apartmentTypes";
+import { State } from "../types/apartmentTypes";
 
-export const useCity = (id = '') => {
-    const [city, setCity] = useState<City>()
+export const useLocationState = (id = '') => {
+    const [locationState, setLocationState] = useState<State>()
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(false);
     const versionAPI = import.meta.env.VITE_APP_API_VERSION;
     const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
-    const resource = import.meta.env.VITE_APP_API_RESOURCE_CITY;
+    const resource = import.meta.env.VITE_APP_API_RESOURCE_STATES;
     const url = `${baseURL}/${versionAPI}/${resource}/${id}` || ''
 
     useEffect(() => {
@@ -15,7 +15,7 @@ export const useCity = (id = '') => {
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                setCity(data)
+                setLocationState(data)
             })
             .catch(error => {
                 setError(error)
@@ -23,5 +23,5 @@ export const useCity = (id = '') => {
             .finally(() => setLoading(false))
     }, [url])
 
-    return { city, loading, error }
+    return { locationState, loading, error }
 }
