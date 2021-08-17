79d492695c06f653a7417d3d3e940ce91f4c2225
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-17 01:24:09 -0400

    create useAttracts

---
+++ src/hooks/useAttracts.ts
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+import { Apartment } from "../types/apartmentTypes";
+
+export const useAttracts = (ap_apartment = 0, fe_feature = 0) => {
+    const [apartment, setApartment] = useState<Apartment>()
+    const [errorApartment, setErrorApartment] = useState(null)
+    const [loadingApartment, setLoadingApartment] = useState(false);
+    const versionAPI = import.meta.env.VITE_APP_API_VERSION;
+    const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
+    const resource = import.meta.env.VITE_APP_API_RESOURCE_APARTMENTS;
+    const url = `${baseURL}/${versionAPI}/${resource}/${ap_apartment}` || ''
+
+    useEffect(() => {
+        setLoadingApartment(true)
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setApartment(data)
+            })
+            .catch(error => {
+                setErrorApartment(error)
+            })
+            .finally(() => setLoadingApartment(false))
+    }, [url])
+
+    return { apartment, loadingApartment, errorApartment }
+}
