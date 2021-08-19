f9e836fdadac22b1d3db525f0aafd73ef36a51ca
Alejandro M. Coca <alxmcr@outlook.com>
2021-08-18 22:25:48 -0400

    Fix issue AparmentCard

--- src/components/cards/ApartmentCard/index.tsx
+++ src/components/cards/ApartmentCard/index.tsx
@@ -22,37 +22,43 @@ export const ApartmentCard = ({ apartment }: ApartmentCardProps) => {
     return <p>There was an error with this neighborhood.</p>;
 
   return (
-    <div
-      className="apartmentcard"
-      id={`${apartment?.ap_apartment}`}
-      onClick={redirectToApartPage}
-    >
-      <div className="apartmentcard__body">
-        <img
-          src={apartment?.ap_url}
-          alt={apartment?.ap_alt}
-          className="apartmentcard__image"
-          loading="lazy"
-        />
-      </div>
-      <div className="apartmentcard__footer">
-        <p className="apartmentcard__cost">
-          <span className="apartmentcard__cost--offer">
-            {apartment?.ap_cost_offer}
-          </span>
-          <span className="apartmentcard__cost--list">
-            {apartment?.ap_cost_list}
-          </span>
-        </p>
-        {!loadingNeighborhood ? (
-          <ApartmentAddress
-            ap_street_name={apartment.ap_street_name}
-            ap_ext_number={apartment.ap_ext_number}
-            neighborhood={neighborhood}
-          />
-        ) : null}
-        <ApartmentAttractIconsGroup ap_apartment={apartment?.ap_apartment} />
-      </div>
-    </div>
+    <>
+      {neighborhood ? (
+        <div
+          className="apartmentcard"
+          id={`${apartment?.ap_apartment}`}
+          onClick={redirectToApartPage}
+        >
+          <div className="apartmentcard__body">
+            <img
+              src={apartment?.ap_url}
+              alt={apartment?.ap_alt}
+              className="apartmentcard__image"
+              loading="lazy"
+            />
+          </div>
+          <div className="apartmentcard__footer">
+            <p className="apartmentcard__cost">
+              <span className="apartmentcard__cost--offer">
+                {apartment?.ap_cost_offer}
+              </span>
+              <span className="apartmentcard__cost--list">
+                {apartment?.ap_cost_list}
+              </span>
+            </p>
+            {!loadingNeighborhood ? (
+              <ApartmentAddress
+                ap_street_name={apartment.ap_street_name}
+                ap_ext_number={apartment.ap_ext_number}
+                neighborhood={neighborhood}
+              />
+            ) : null}
+            <ApartmentAttractIconsGroup
+              ap_apartment={apartment?.ap_apartment}
+            />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
