bcdada4b7026b41c85a4d1dae498848501be10eb
Alejandro M. Coca <alxmcr@outlook.com>
2024-01-04 13:51:29 -0400

    wip

--- src/api/ApartmentsImpl.ts
+++ src/api/ApartmentsImpl.ts
@@ -3,18 +3,19 @@ import { IApartments } from "./IApartments";
 
 export class ApartmentsImpl implements IApartments {
   private urlAPI: string;
+  private abortController: AbortController;
 
-  constructor() {
+  constructor(apiAbort: AbortController) {
     const versionAPI = import.meta.env.VITE_APP_API_VERSION;
     const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
 
     this.urlAPI = `${baseURL}/${versionAPI}/apartments` || "";
+    this.abortController = apiAbort;
   }
 
   async findAll() {
     // Abort fetch
-    let abortController = new AbortController();
-    const { signal } = abortController;
+    const { signal } = this.abortController;
 
     const response = await fetch(this.urlAPI, { signal });
     const body = await response.json();
