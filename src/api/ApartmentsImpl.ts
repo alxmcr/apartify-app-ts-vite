f02163f0376c22e0d0b3dcd83e3d9c8cac0d7fe1
Alejandro M. Coca <alxmcr@outlook.com>
2024-01-04 13:49:50 -0400

    wip

--- src/api/ApartmentsImpl.ts
+++ src/api/ApartmentsImpl.ts
@@ -1,17 +1,22 @@
 import { Apartment } from "../@types/apartmentTypes";
-import { appHttp } from "../helpers/appHttp";
 import { IApartments } from "./IApartments";
 
 export class ApartmentsImpl implements IApartments {
-  async findAll() {
+  private urlAPI: string;
+
+  constructor() {
     const versionAPI = import.meta.env.VITE_APP_API_VERSION;
     const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
-    const url = `${baseURL}/${versionAPI}/apartments` || "";
+
+    this.urlAPI = `${baseURL}/${versionAPI}/apartments` || "";
+  }
+
+  async findAll() {
     // Abort fetch
     let abortController = new AbortController();
     const { signal } = abortController;
 
-    const response = await fetch(url, { signal });
+    const response = await fetch(this.urlAPI, { signal });
     const body = await response.json();
 
     return body as Promise<Apartment[]>;
