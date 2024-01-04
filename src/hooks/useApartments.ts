ce5b5db7fb82e05538ad535d26cb4ccb14822fc8
Alejandro M. Coca <alxmcr@outlook.com>
2024-01-04 13:57:35 -0400

    hook update

--- src/hooks/useApartments.ts
+++ src/hooks/useApartments.ts
@@ -1,37 +1,44 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 import { appHttp } from "../helpers/appHttp";
 import { Apartment } from "../@types/apartmentTypes";
+import { ApartmentsServiceImpl } from "../api/ApartmentsServiceImpl";
 
 export const useApartments = () => {
-    const [apartments, setApartments] = useState<Apartment[]>([])
-    const [errorApartments, setErrorApartments] = useState(null)
-    const [loadingApartments, setLoadingApartments] = useState(false);
-    const versionAPI = import.meta.env.VITE_APP_API_VERSION;
-    const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
-    const url = `${baseURL}/${versionAPI}/apartments` || ''
+  const [apartments, setApartments] = useState<Apartment[]>([]);
+  const [errorApartments, setErrorApartments] = useState<Error | null>(null);
+  const [loadingApartments, setLoadingApartments] = useState(false);
+  const versionAPI = import.meta.env.VITE_APP_API_VERSION;
+  const baseURL = import.meta.env.VITE_APP_API_BASE_URL;
+  const url = `${baseURL}/${versionAPI}/apartments` || "";
 
-    useEffect(() => {
-        // Abort fetch
-        let abortController = new AbortController();
-        const { signal } = abortController;
+  useEffect(() => {
+    // Abort fetch
+    let abortController = new AbortController();
 
-        setLoadingApartments(true)
-        appHttp<Apartment[]>(url, signal)
-            .then(data => {
-                setApartments(data)
-            })
-            .catch(error => {
-                if (error.name !== 'AbortError') {
-                    setErrorApartments(error)
-                }
-            })
-            .finally(() => setLoadingApartments(false))
+    const fetchApartments = async () => {
+      try {
+        setLoadingApartments(true);
 
-        // Clean up
-        return function cancel() {
-            abortController?.abort();
-        };
-    }, [url])
+        const service = new ApartmentsServiceImpl(abortController);
+        const apiApartments = await service.findAll();
+        setApartments(apiApartments);
+      } catch (error) {
+        if (error instanceof Error) {
+          setErrorApartments(error);
+        }
+      } finally {
+        setLoadingApartments(false);
+      }
+    };
 
-    return { apartments, loadingApartments, errorApartments }
-}
+    // Fetch
+    fetchApartments();
+
+    // Clean up
+    return function cancel() {
+      abortController?.abort();
+    };
+  }, [url]);
+
+  return { apartments, loadingApartments, errorApartments };
+};
